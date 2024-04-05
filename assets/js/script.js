6977097a334f0732ace3ad71024c8a4c6f41c17b
hysinh <156194500+hysinh@users.noreply.github.com>
2024-04-05 14:05:12 +0000

    Adds dataset to the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,18 @@ const playerLivesCount = document.getElementById('score');
 let playerLives = 20;
 playerLivesCount.innerHTML = playerLives;
 
+// The Data
+let cardData = [
+    { imgSrc: "assets/images/beatles.jpeg", name: "beatles" },
+    { imgSrc: "assets/images/blink182.jpeg", name: "blink 182" },
+    { imgSrc: "assets/images/fkatwigs.jpeg", name: "fka twigs" },
+    { imgSrc: "assets/images/fleetwood.jpeg", name: "fleetwood" },
+    { imgSrc: "assets/images/joy-division.jpeg", name: "joy division" },
+    { imgSrc: "assets/images/ledzep.jpeg", name: "lep zeppelin" },
+    { imgSrc: "assets/images/metallica.jpeg", name: "metallica" },
+    { imgSrc: "assets/images/pinkfloyd.jpeg", name: "pink floyd" },
+];
+
 function shuffleCards() {
 
 }
