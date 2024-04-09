b507c13f1ca35018761b7ce32f088ba446a35b0a
hysinh <156194500+hysinh@users.noreply.github.com>
2024-04-09 13:01:11 +0000

    Updates the card images with new images

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,14 +17,14 @@ playerLivesCount.innerHTML = playerLives;
 
 // The Data
 let cardData = [
-    { imgSrc: "assets/images/beatles.jpeg", name: "beatles" },
-    { imgSrc: "assets/images/blink182.jpeg", name: "blink 182" },
-    { imgSrc: "assets/images/fkatwigs.jpeg", name: "fka twigs" },
-    { imgSrc: "assets/images/fleetwood.jpeg", name: "fleetwood" },
-    { imgSrc: "assets/images/joy-division.jpeg", name: "joy division" },
-    { imgSrc: "assets/images/ledzep.jpeg", name: "lep zeppelin" },
-    { imgSrc: "assets/images/metallica.jpeg", name: "metallica" },
-    { imgSrc: "assets/images/pinkfloyd.jpeg", name: "pink floyd" },
+    { imgSrc: "assets/images/card_happyvalley_egg.webp", name: "egg" }, 
+    { imgSrc: "assets/images/card_happyvalley_grumpy.webp", name: "grumpy" },
+    { imgSrc: "assets/images/card_happyvalley_leafy.webp", name: "leafy" },
+    { imgSrc: "assets/images/card_happyvalley_pete.webp", name: "pete" },
+    { imgSrc: "assets/images/card_happyvalley_pisa.webp", name: "pisa" },
+    { imgSrc: "assets/images/card_happyvalley_poppy.webp", name: "poppy" },
+    { imgSrc: "assets/images/card_happyvalley_smiley.webp", name: "smiley" },
+    { imgSrc: "assets/images/card_happyvalley_walter.webp", name: "walter" },
 ];
 
 // duplicates the data
